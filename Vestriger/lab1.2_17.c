a765f34078e44ebf5dca803df934a02a9061e588
Pavel <78044591+Vestriger@users.noreply.github.com>
2021-03-23 15:04:45 +0300

    First commit (#1)

---
+++ Vestriger/lab1.2_17.c
@@ -0,0 +1,22 @@
+﻿//Геращенко Павел 053505 Лаб. работа №1, вариант 17
+
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c;
+	for (int i = 100; i < 1000; i++)
+	{
+		a = pow((i/100),3);
+		b = pow((i % 100 / 10), 3);
+		c = pow((i % 100 % 10), 3);
+		if (i == a + b + c)
+		{
+			printf("%d\t", i);
+		}
+	}
+
+
+	return 0;
+}
