a1edd38b2a3110c9b9ade7619508104522a093e2
dzmitryyermalovich <79224710+dzmitryyermalovich@users.noreply.github.com>
2021-04-06 14:12:20 +0300

    lab1 Вариант 14 (#3)

---
+++ dzmitryyermalovich/lab1/lab1/lab1.cpp
@@ -0,0 +1,53 @@
+﻿//Ермолович Дима 053505 Лаб. работа №1, вариант 14
+
+#include <iostream>
+#include<math.h>
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+
+	long long int n;
+	printf("n = ");
+	scanf_s("%lld", &n);
+	long long int currantValue  = 2;
+	long long int newValue = 2;
+	int metka = 0;
+	
+	for (int index = 2; index <= 1000; index++)
+	{
+		newValue = pow(2, index) - 1;
+
+	for (long long int j = 2; j <= (newValue /2); j++)
+	{
+		if (newValue % j == 0)
+		{
+			metka = 1;
+			break;
+		}
+	}
+
+	
+		if (metka!=1) 
+		{
+			currantValue = pow(2, (index - 1)) * (pow(2, index) - 1);
+
+			if (n >= currantValue) 
+			{
+				printf("%lld\n", currantValue);
+			}
+			else 
+			{
+				break;
+			}
+		}
+ 
+
+
+		metka = 0;
+	}
+
+
+
+}
